eebd3a567e2cabf77d45486701447cc3d7d10330
Yi-Jun-Wu <wuyijun21@mails.ucas.edu.cn>
2026-03-20 12:07:09 +0800

    1

--- check-lectures/get_all_replies.ts
+++ check-lectures/get_all_replies.ts
@@ -69,11 +69,12 @@ export async function get_all_replies(
   return allReplies;
 }
 
-// test
-const token = process.env.DISCUSSION_READ_TOKEN;
-if (!token || token.length === 0) {
-  throw new Error("请设置 DISCUSSION_READ_TOKEN 环境变量");
+if(import.meta.main) {
+  const token = process.env.DISCUSSION_READ_TOKEN;
+  if (!token || token.length === 0) {
+    throw new Error("请设置 DISCUSSION_READ_TOKEN 环境变量");
+  }
+  const commentId = "DC_kwDORqBAz84A93qz";
+  const content = await get_all_replies(commentId, token);
+  console.log(content);
 }
-const commentId = "DC_kwDORqBAz84A93qz";
-const content = await get_all_replies(commentId, token);
-console.log(content);
